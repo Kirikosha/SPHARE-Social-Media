d8c4df02235564b5edab14a9aaeda6518365ca93
Kirikosha <kirikoshamain@gmail.com>
2026-03-24 20:08:26 +0100

    (feature): optimised GetPublicUserByUniqueNameIdentifier

--- src/Application/Features/Users/Queries/GetPublicUserByUniqueNameIdentifier.cs
+++ src/Application/Features/Users/Queries/GetPublicUserByUniqueNameIdentifier.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.Features.Users.Queries;
+﻿using Domain.DTOs.DetailedUserInfoDTOs;
+
+namespace Application.Features.Users.Queries;
 
 using Core;
 using AutoMapper;
@@ -15,20 +17,47 @@ public class GetPublicUserByUniqueNameIdentifier
         public required string UniqueNameIdentifier { get; set; }
     }
 
+    //TODO: think about some sort of repository pattern since we are duplicating code here and in GetPublicUserById
     public class Handler(ApplicationDbContext context, IMapper mapper) 
         : IRequestHandler<Query, Result<PublicUserDto>>
     {
         public async Task<Result<PublicUserDto>> Handle(Query request, CancellationToken cancellationToken)
         {
-            User? user = await context.Users
-                .Include(a => a.ProfileImage)
-                .Include(a => a.Address)
-                .Include(a => a.ProfileDetails)
-                .FirstOrDefaultAsync(a => a.UniqueNameIdentifier == request.UniqueNameIdentifier,
-                    cancellationToken);
+            var userDto = await context.Users
+                .Where(u => u.UniqueNameIdentifier == request.UniqueNameIdentifier)
+                .Select(u => new PublicUserDto
+                {
+                    Id = u.Id,
+                    Username = u.Username,
+                    UniqueNameIdentifier = u.UniqueNameIdentifier,
+                    JoinedAt = u.DateOfCreation.ToString("yyyy-MM-dd"),
+                    ProfileImage = u.ProfileImage != null ? u.ProfileImage.ImageUrl : null,
+                    Blocked = u.Blocked,
+                    UserProfileDetails = u.ProfileDetails != null
+                        ? new UserProfileDetailsDto
+                        {
+                            Id = u.ProfileDetails.Id,
+                            Pronouns = u.ProfileDetails.Pronouns,
+                            MainProfileDescription = u.ProfileDetails.MainProfileDescription,
+                            Interests = u.ProfileDetails.Interests,
+                            DateOfBirth = u.ProfileDetails.DateOfBirth
+                        }
+                        : null,
+                    Address = u.Address != null
+                        ? new AddressDto
+                        {
+                            Id = u.Address.Id,
+                            City = u.Address.City,
+                            Country = u.Address.Country
+                        }
+                        : null
+                })
+                .FirstOrDefaultAsync(cancellationToken);
 
-            return user == null ? Result<PublicUserDto>.Failure("User was not found", 404) 
-                : Result<PublicUserDto>.Success(mapper.Map<PublicUserDto>(user));
+            if (userDto == null)
+                return Result<PublicUserDto>.Failure("User was not found", 404);
+
+            return Result<PublicUserDto>.Success(userDto);
         }
     }
 }

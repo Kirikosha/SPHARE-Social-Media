7cd3e4917f8bc62ef0588d838ea42e715de517da
Kirikosha <kirikoshamain@gmail.com>
2026-05-03 20:13:10 +0200

    (fix): forgot to add this to the previous commit

---
+++ server/src/Application/Features/Publications/Commands/UpdateConditionalPublication.cs
@@ -0,0 +1,23 @@
+﻿using Application.Core;
+using Application.DTOs.PublicationDTOs;
+using Application.Interfaces.Services;
+
+namespace Application.Features.Publications.Commands;
+
+public class UpdateConditionalPublication
+{
+    public class Command : IRequest<Result<PublicationDto>>
+    {
+        public required string UserId { get; set; }
+        public required UpdateConditionalPublicationDto UpdateDto { get; set; }
+    }
+    
+    public class Handler(IPublicationService publicationService) : IRequestHandler<Command, Result<PublicationDto>>
+    {
+        public async Task<Result<PublicationDto>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            return await publicationService.UpdateConditionalPublicationAsync(request.UpdateDto, request.UserId,
+                cancellationToken);
+        }
+    }
+}
